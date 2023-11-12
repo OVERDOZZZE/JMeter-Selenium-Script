1c1ee67fb21945aca005db0e1da21f9052004d5c
OVERDOZZZE <saparbekov113@gmail.com>
2023-11-12 20:47:57 +0600

    commit

--- funcs.py
+++ funcs.py
@@ -1,9 +1,12 @@
 import git
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 100)
+
 
 
 def write_down(code, filename):
@@ -33,12 +36,25 @@ def leetcode_login(login, password):
 
     login_button = driver.find_element(By.ID, 'signin_btn')
     login_button.click()
-    wait = WebDriverWait(driver, 100)
     wait.until(EC.url_matches("https://leetcode.com/"))
-    
 
-def leetcode_copy_code(problem):    
-    driver.get(problem)
+
+def get_leetcode_solution(day):
+    driver.get('https://leetcode.com/problemset/all/?difficulty=EASY&page=1')
+    time.sleep(2)
+    problem_list = driver.find_elements(By.XPATH, "//div[@role='rowgroup']//div[@role='cell']//a")
+    problem_list[day].click()
+    time.sleep(5)
+
+    button = driver.find_element(By.XPATH, f"//span[contains(text(), '{'Solutions'}')]/..")
+    button.click()
+    time.sleep(5)
+
+    solutions = driver.find_elements(By.XPATH, '//div[@class="group flex w-full cursor-pointer flex-col gap-1.5 px-4 pt-3"]')
+    solutions[1].click()
+    time.sleep(5)
+
     code = driver.find_elements(By.CSS_SELECTOR, 'code.language-cpp')
 
     return code
+
--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,21 @@
-from funcs import write_down, push_to_github, leetcode_copy_code, leetcode_login
+from funcs import write_down, push_to_github, get_leetcode_solution
 
-DAY = 0
-LOCAL_REPO = r"C:\Users\User\PycharmProjects\Selenim-Jmet\Selenium-JMeter\github"
+DAY = 4
+LOCAL_REPO = r"C:\Users\User\PycharmProjects\Selenim-Jmet\Selenium-JMeter\hub"
 PROBLEM_PATH = 'https://leetcode.com/problems/two-sum/solutions/3619262/3-method-s-c-java-python-beginner-friendly/'
 
 
 if __name__ == '__main__':
     try:
-        leetcode_login('saparbekov113@gmail.com', 'nurdan2005.')
-        code = leetcode_copy_code(PROBLEM_PATH)
+        code = get_leetcode_solution(day=DAY)
         write_down(
-            code[DAY].text, 'github/hub.cpp'
+            code[0].text, 'hub/hub.cpp'
         )
         push_to_github(
             repository_path = LOCAL_REPO,
             file_path = "hub.cpp",
             commit_message = f"commit-{DAY}",
         )
-        print(len(code))
         print('Success!')
     except Exception as e:
         print(e)
